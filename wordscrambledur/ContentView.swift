2b089182f6eefc838f883edf2d99acfdf120baaa
yannemal <56878180+Yannemal@users.noreply.github.com>
2023-07-12 12:14:41 +0800

    List id: \.self

--- wordscrambledur/ContentView.swift
+++ wordscrambledur/ContentView.swift
@@ -9,30 +9,35 @@ import SwiftUI
 
 struct ContentView: View {
     // MARK: - DATA
-    
+    let starWarsCharacters = ["Luke Skywalker", "Darth Vader", "Princess Leia Organa", "Han Solo", "Chewbacca", "C-3PO", "R2-D2", "Obi-Wan Kenobi", "Yoda"]
+
     var body: some View {
-        //List 1
-        List{
-            Text("Hello Earth")
-            Text("Hello Moon")
-            Text("Hello International SpaceStation")
-            
-            Section("dynamic bit")  {
-                ForEach(0..<5) {
-                    Text("Dynamic Row of List \($0)")
-                }
-            }
-            Section {
-                Text("mix and match sections in one List but not Form: thats a special kind of list. 1 list per view")
-            }
-            
-            Text("another hardcoded row")
-            Text("another hardcoded row, no problem")
-        }
-        .listStyle(.sidebar)
+        //example List 1
+        
+//        List{
+//            Text("Hello Earth")
+//            Text("Hello Moon")
+//            Text("Hello International SpaceStation")
+//
+//            Section("dynamic bit")  {
+//                ForEach(0..<5) {
+//                    Text("Dynamic Row of List \($0)")
+//                }
+//            }
+//            Section {
+//                Text("mix and match sections in one List but not Form: thats a special kind of list. 1 list per view")
+//            }
+//
+//            Text("another hardcoded row")
+//            Text("another hardcoded row, no problem")
+//        }
+//        .listStyle(.sidebar)
       
+        //example List 2
         
-        
+        List(starWarsCharacters.sorted(), id: \.self) {
+            Text($0)
+        } 
         
     } //end body View
 } // ContentView
