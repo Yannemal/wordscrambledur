357b00ad84bc2d54b7ab7aae215cc06b03154820
yannemal <56878180+Yannemal@users.noreply.github.com>
2023-07-13 01:13:45 +0800

    Got rudimentary version 1 working
    Fix method: Strings can still have spaces .. method doesnt work properly.
    .fill on even for .circle doesnt work : ForEach changes all fo them ?

--- wordscrambledur/ContentView.swift
+++ wordscrambledur/ContentView.swift
@@ -10,39 +10,81 @@ import SwiftUI
 struct ContentView: View {
     // MARK: - DATA
     let starWarsCharacters = ["Luke Skywalker", "Darth Vader", "Princess Leia Organa", "Han Solo", "Chewbacca", "C-3PO", "R2-D2", "Obi-Wan Kenobi", "Yoda"]
-
-    var body: some View {
-        //example List 1
+    // collection of words earlier attempts
+    @State private var usedWords = [String]()
+    // will be word player is spelling from
+    @State private var rootWord = ""
+    // new attempt by player to bind to textfield
+    @State private var newWord = ""
+    
+    // if array has an even number of indexes set to true
+    private var even : Bool {
+        return usedWords.count % 2 == 0
+    }
         
-//        List{
-//            Text("Hello Earth")
-//            Text("Hello Moon")
-//            Text("Hello International SpaceStation")
-//
-//            Section("dynamic bit")  {
-//                ForEach(0..<5) {
-//                    Text("Dynamic Row of List \($0)")
-//                }
-//            }
-//            Section {
-//                Text("mix and match sections in one List but not Form: thats a special kind of list. 1 list per view")
-//            }
-//
-//            Text("another hardcoded row")
-//            Text("another hardcoded row, no problem")
-//        }
-//        .listStyle(.sidebar)
+        
+    
+    var body: some View {
+
+        NavigationStack {
+            List {
+                Section {
+                    TextField("Enter your word", text: $newWord)
+                        .textInputAutocapitalization(.never)
+                        .autocorrectionDisabled()
+                }
+                
+                Section {
+                    ForEach(usedWords, id: \.self) { word in
+                        HStack{
+                          
+                            Image(systemName: even ? "\(word.count).circle" : "\(word.count).circle.fill")
+                            Text(word)
+                          }
+                        }
+                    }
+                
+            } // end NavStack
+            .navigationTitle(rootWord)
+            .onSubmit(addNewWord)
+        }
       
         //example List 2
         
-        List(starWarsCharacters.sorted(), id: \.self) {
-            Text($0)
-        } 
+//        List(starWarsCharacters.sorted(), id: \.self) {
+//            Text($0)
+//        }
         
     } //end body View
+    
+    // MARK: - METHODS
+    func addNewWord() {
+        // taking the newWord from TextField entry > lowerCase > no whiteSpace
+        let answer = newWord.lowercased()
+        let answer2 = answer.trimmingCharacters(in: .whitespacesAndNewlines)
+        // make sure theres a minimum requirement
+        guard answer2.count > 0 else { return }
+        // add answer to array at 0 so it comes in at the top of the List always in view
+        withAnimation {
+            usedWords.insert(answer2, at: 0)
+        }
+            //reset textField
+        newWord = ""
+        
+        
+    }
+    
+    
+    func loadFile() {
+        if let fileURL = Bundle.main.url(forResource: "some-file", withExtension: "txt") {
+            if let fileContents = try? String(contentsOf: fileURL) {
+                // we loaded the file into a String
+            }
+        }
+    }
+    
+    
 } // ContentView
-
-// MARK: - METHODS
 
 
 // MARK: - PREVIEW
